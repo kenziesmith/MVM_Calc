e607f8241ba1ccde9e856fbcf7f29346ccdad2fb
Kenzie <kenziebell5928@gmail.com>
2024-02-22 14:50:06 +0300

    Добавлена кнопка очистки, внесены мелкие правки в код

--- script.js
+++ script.js
@@ -56,23 +56,28 @@ function createForm() {
   const formInpAcessories = createFormInput('Аксессуары');
   const formInpServices = createFormInput('Услуги');
 
+  formInpChecks.input.value = 10
+  formInpTurnover.input.value = 100000
+  formInpAcessories.input.value = 7000
+  formInpServices.input.value = 4500
+
   formInpChecks.input.placeholder = 'Количество чеков...'
   formInpTurnover.input.placeholder = 'Пример: 300000...'
-  formInpAcessories.input.placeholder = 'Пример: 7999+999+499...'
-  formInpServices.input.placeholder = 'Пример: 2999+1990+499...'
+  formInpAcessories.input.placeholder = 'Пример: 999+999+999...'
+  formInpServices.input.placeholder = 'Пример: 999+999+999...'
 
   const fromBtnWrapper = document.createElement('div');
   const formBtn = document.createElement('button');
+  const formBtnClear = document.createElement('button');
 
   form.className = 'input-group mb-3 mt-4';
   formBtn.className = 'btn form-btn btn-success';
+  formBtnClear.className = 'btn form-btn btn-danger mb-2 form-btn-clear';
   formBtn.textContent = 'Применить';
   fromBtnWrapper.style.width = '100%';
-  formBtn.style.width = '100%';
-  formBtn.style.padding = '10px';
-  formBtn.style.fontSize = '18px';
+  formBtnClear.textContent = 'Очистить'
 
-  fromBtnWrapper.append(formBtn);
+  fromBtnWrapper.append(formBtnClear, formBtn);
 
   form.append(formInpChecks.inputBox, formInpTurnover.inputBox, formInpAcessories.inputBox, formInpServices.inputBox, fromBtnWrapper);
 
@@ -83,11 +88,17 @@ function createForm() {
     formInpAcessories,
     formInpServices,
     formBtn,
+    formBtnClear,
   }
 }
 
-function calculatePercentage(a, b) {
-  return b / a * 100;
+// функция вычисляет процент значение от общего оборота продаж
+const calculatePercentage = (a, b) => b / a * 100;
+
+function clearInputs(inputsArray) {
+  for (let i = 1; i <= inputsArray.length; i++) {
+    inputsArray[i - 1].input.value = '';
+  }
 }
 
 // сборка приложения
@@ -96,6 +107,11 @@ const app = document.getElementById('app');
 const appContainer = document.createElement('div'); // контейнер
 const formGroup = document.createElement('div'); // обёртка для формы
 const appForm = createForm(); // форма
+
+const checks = appForm.formInpChecks;
+const turnover = appForm.formInpTurnover;
+const acessories = appForm.formInpAcessories;
+const services = appForm.formInpServices;
 
 appContainer.className = 'container';
 formGroup.style.display = 'flex';
@@ -106,11 +122,6 @@ app.append(appContainer);
 
 appForm.formBtn.addEventListener('click', (e) => {
   e.preventDefault();
-
-  const checks = appForm.formInpChecks;
-  const turnover = appForm.formInpTurnover;
-  const acessories = appForm.formInpAcessories;
-  const services = appForm.formInpServices;
 
   navigator.clipboard.writeText(`Чеки: ${checks.format()}
 Оборот: ${turnover.format()}
@@ -125,4 +136,9 @@ appForm.formBtn.addEventListener('click', (e) => {
 Оборот: ${turnover.format()}
 Аксессуары: ${acessories.format()} (${calculatePercentage(turnover.calculate(), acessories.calculate()).toFixed(2)}%)
 Услуги: ${services.format()} (${calculatePercentage(turnover.calculate(), services.calculate()).toFixed(2)}%)`);
+});
+
+appForm.formBtnClear.addEventListener('click', (e) => {
+  e.preventDefault();
+  clearInputs([checks, turnover, acessories, services]);
 });
